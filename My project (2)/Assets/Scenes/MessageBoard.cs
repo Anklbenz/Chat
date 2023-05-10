b6aed2b6dce695d1faf83ecb1181ca13a351efe3
marukh vyacheslav <76220960+Anklbenz@users.noreply.github.com>
2023-05-11 01:34:38 +0300

    kk

---
+++ My project (2)/Assets/Scenes/MessageBoard.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MessageBoard : MonoBehaviour
+{
+   [SerializeField] private Message userMessagePrefab, companionMessagePrefab;
+   [SerializeField] private RectTransform messageParent;
+
+   public void CreateUserMessage() {
+      
+   }
+
+   public void CreateCompanionMessage() {
+      
+   }
+
+}
